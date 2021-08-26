e61be37d55f9ce8ba2430bc5ded11a89ae47ce3f
Dean Bandes <deanlie@yahoo.com>
2021-08-26 17:11:37 -0400

    Working to replace discardTooNewData...
    Add discardTooNewData2FromAFile, convenience routine for calling ..outsideDateRange... with NULL for firstDateToKeep
    Change logic of OutsideDateRange to avoid problem with NULL args
    Get test data from cache for repeatability (discard routine overwrites original file)

--- testDiscardNewDataFilewise.R
+++ testDiscardNewDataFilewise.R
@@ -51,6 +51,27 @@ discardTooNewDataFromAFile <- function(thePath,
   return(list(T0 = originalData, T1 = truncatedTibble))
 }
 
+discardTooNewData2FromAFile <- function(thePath,
+                                       theTypes,
+                                       firstDateToDelete,
+                                       traceThisRoutine = FALSE, prepend = "",
+                                       theFileName = "UNKNOWN") {
+  myPrepend = paste("  ", prepend, sep = "")  
+  if (traceThisRoutine) {
+    cat(file = stderr(), prepend, "Entered discardTooNewData2FromAFile", theFileName, "\n")
+  }
+
+  return_me <- discardDataOutsideDateRangeFromAFile(thePath,
+                                              theTypes,
+                                              NULL,
+                                              firstDateToDelete - 1,
+                                              traceThisRoutine, myPrepend)
+  if (traceThisRoutine) {
+    cat(file = stderr(), prepend, "Leaving discardTooNewData2FromAFile", theFileName, "\n")
+  }
+  
+  return(return_me)
+}
 
 discardDataOutsideDateRangeFromAFile <- function(thePath,
                                                  theTypes,
@@ -87,9 +108,22 @@ discardDataOutsideDateRangeFromAFile <- function(thePath,
   newNames <- charNames
   for (aName in dateNames) {
     aDate <- mdy(aName)
-    if ((is.null(firstDateToKeep) | (aDate >= firstDateToKeep)) &
-      (is.null(lastDateToKeep) | (aDate <= lastDateToKeep))) {
-      newNames <- c(newNames, aName)
+    if (is.null(firstDateToKeep)) {
+      if (!is.null(lastDateToKeep)) {
+        if (aDate <= lastDateToKeep) {
+          newNames <- c(newNames, aName)
+        }
+      }
+    } else {
+      if (aDate >= firstDateToKeep) {
+        if (is.null(lastDateToKeep)) {
+          newNames <- c(newNames, aName)
+        } else {
+          if (aDate <= lastDateToKeep) {
+            newNames <- c(newNames, aName)
+          }
+        }
+      }
     }
   }
   
@@ -105,30 +139,66 @@ discardDataOutsideDateRangeFromAFile <- function(thePath,
   return(list(T0 = originalData, T1 = truncatedTibble))
 }
 
+reinitVaccTestData <- function() {
+  system("cp DATA/CACHE/US_Vaccinations.csv DATA/")
+}
+
+reinitIRTestData <- function() {
+  system("cp DATA/CACHE/US_Incident_Rate.csv DATA/")
+}
+
 testDiscardTooNew_US_vacc <- function(traceThisRoutine = FALSE) {
+  reinitVaccTestData()
   foo1 <- discardTooNewDataFromAFile("./DATA/US_Vaccinations.csv",
                                      cols(.default = col_double(),
                                           Combined_Key = col_character(),
                                           Datum = col_character(),
                                           Loc_Datum = col_character()), 
                                      mdy("08-13-2021"),
-                                     traceThisRoutine = traceThisRoutine, prepend = "TEST",
+                                     traceThisRoutine = traceThisRoutine, prepend = "TEST_V  ",
                                      theFileName = "US_Vaccinations.csv")
   return(foo1)
 }
 
+testDiscardTooNew2_US_vacc <- function(traceThisRoutine = FALSE) {
+  reinitVaccTestData()
+  foo1 <- discardTooNewData2FromAFile("./DATA/US_Vaccinations.csv",
+                                      cols(.default = col_double(),
+                                           Combined_Key = col_character(),
+                                           Datum = col_character(),
+                                           Loc_Datum = col_character()), 
+                                      mdy("08-13-2021"),
+                                      traceThisRoutine = traceThisRoutine, prepend = "TEST_V2 ",
+                                      theFileName = "US_Vaccinations.csv")
+  return(foo1)
+}
+
 testDiscardTooNew_US_I_R <- function(traceThisRoutine = FALSE) {
+  reinitIRTestData()
   foo1 <- discardTooNewDataFromAFile("./DATA/US_Incident_Rate.csv",
                                      cols(.default = col_double(),
                                           Province_State = col_character(),
                                           Combined_Key = col_character()), 
                                      mdy("07-15-2021"),
-                                     traceThisRoutine = traceThisRoutine, prepend = "TEST",
+                                     traceThisRoutine = traceThisRoutine, prepend = "TEST_I_R",
                                      theFileName = "US_Incident_Rate.csv")
   return(foo1)
 }
 
+testDiscardTooNew2_US_I_R <- function(traceThisRoutine = FALSE) {
+  reinitIRTestData()
+  foo1 <- discardTooNewData2FromAFile("./DATA/US_Incident_Rate.csv",
+                                      cols(.default = col_double(),
+                                           Province_State = col_character(),
+                                           Combined_Key = col_character()), 
+                                      mdy("07-15-2021"),
+                                      traceThisRoutine = traceThisRoutine, prepend = "TEST_I_R",
+                                      theFileName = "US_Incident_Rate.csv")
+  return(foo1)
+}
+
 testDiscardDataOutsideDateRange_US_vacc <- function(traceThisRoutine = FALSE) {
+  reinitVaccTestData()
   foo1 <- discardDataOutsideDateRangeFromAFile("./DATA/US_Vaccinations.csv",
                                      cols(.default = col_double(),
                                           Combined_Key = col_character(),
@@ -142,6 +212,7 @@ testDiscardDataOutsideDateRange_US_vacc <- function(traceThisRoutine = FALSE) {
 }
 
 testDiscardDataOutsideDateRange_US_I_R <- function(traceThisRoutine = FALSE) {
+  reinitIRTestData()
   foo1 <- discardDataOutsideDateRangeFromAFile("./DATA/US_Incident_Rate.csv",
                                      cols(.default = col_double(),
                                           Province_State = col_character(),
@@ -153,6 +224,20 @@ testDiscardDataOutsideDateRange_US_I_R <- function(traceThisRoutine = FALSE) {
   return(foo1)
 }
 
-testSuite <- function() {
+testSuite <- function(traceThisRoutine = FALSE) {
   foo1 <- testDiscardDataOutsideDateRange_US_vacc(traceThisRoutine = traceThisRoutine)
+  foo2 <- testDiscardTooNew_US_vacc(traceThisRoutine = traceThisRoutine)
+  foo3 <- testDiscardTooNew2_US_vacc(traceThisRoutine = traceThisRoutine)
+  foo4 <- testDiscardDataOutsideDateRange_US_I_R(traceThisRoutine = traceThisRoutine)
+  foo5 <- testDiscardTooNew_US_I_R(traceThisRoutine = traceThisRoutine)
+  foo6 <- testDiscardTooNew2_US_I_R(traceThisRoutine = traceThisRoutine)
+  return(list(ORIG_123 = foo1$T0
+              , RES1 = foo1$T1
+              , RES2 = foo2$T1
+              , RES3 = foo3$T1
+              , ORIG_456 = foo4$T0
+              , RES4 = foo4$T1
+              , RES5 = foo5$T1
+              , RES6 = foo6$T1
+              ))
 }
