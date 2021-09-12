0413520a543d4083f0b8ec0a8d11c49faf6956b7
Dean Bandes <deanlie@yahoo.com>
2021-09-11 22:24:42 -0400

    Merge refactored load mortality rate, testing rate data

--- loadUSMortalityRateData.R
+++ loadUSMortalityRateData.R
@@ -1,16 +1,11 @@
-source("./updateStateLevelSerializedDataFiles.R")
-source("./variableFieldNames.R")
+source("updateStateLevelSerializedDataFiles.R")
+source("columnUtilities.R")
 
 loadUSMortalityRateData <- function(aDate) {
-  vfn <- variableFieldNames(aDate)
-
   updateStateLevelSerializedDataFilesAsNecessary()
   
-  US_Mortality_Rate <<- read_csv(paste("./DATA/US_", vfn$mortality, ".csv", sep = ""),
-                                 col_types = cols(.default = col_double(),
-                                                  Province_State = col_logical(),
-                                                  Combined_Key = col_character()))
-  US_State_Mortality_Rate <<- read_csv(paste("./DATA/US_State_", vfn$mortality, ".csv", sep = ""),
-                                       col_types = cols(.default = col_double(),
-                                                        Combined_Key = col_character()))
+  US_Mortality_Rate <<- read_csv("./DATA/US_Case_Fatality_Ratio.csv",
+                                 col_types = myTSColTypes())
+  US_State_Mortality_Rate <<- read_csv("./DATA/US_State_Case_Fatality_Ratio.csv",
+                                       col_types = justCKColTypes())
 }
