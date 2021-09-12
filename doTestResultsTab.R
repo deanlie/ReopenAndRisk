c0356ddb3a57219baa493c2b07023c870054ad56
Dean Bandes <deanlie@yahoo.com>
2021-09-11 22:09:25 -0400

    Get test result plot titles from a function

--- doTestResultsTab.R
+++ doTestResultsTab.R
@@ -8,6 +8,10 @@ testResultsHeaderHTML <- function(chooseCounty, countyChoices, stateChoices) {
   HTML(theText)
 }
 
+testResultPlotTitle <- function(forBoxplot, justUS, movingAvg) {
+  title <- plotTitle("TestPositivity", forBoxplot, justUS, movingAvg)
+}
+
 plotTestResultBoxplots <- function(chooseCounty, movingAvg, countyChoices,
                                    stateChoices, timeWindow) {
   prepend <- ""
@@ -20,14 +24,14 @@ plotTestResultBoxplots <- function(chooseCounty, movingAvg, countyChoices,
   }
 
   # updateDataForUSTypeIfNeeded("Confirmed")
+  title <- testResultPlotTitle(TRUE, length(stateChoices) == 0, movingAvg)
+  
   if (movingAvg) {
-    title <- "Test Positivity Distribution, 7 day moving average"
     theCaseData <- US_State_Confirmed_A7
     theTestData <- US_State_People_Tested_A7 
     numTibbleName <- "US_State_Confirmed_A7"
     denomTibbleName <- "US_State_People_Tested_A7"
   } else {
-    title <- "Test Positivity Distribution"
     theCaseData <- US_State_Confirmed
     theTestData <- US_State_People_Tested 
     numTibbleName <- "US_State_Confirmed"
@@ -62,23 +66,21 @@ plotTestResultTrend <- function(chooseCounty, movingAvg, countyChoices,
   }
 
   # updateDataForUSTypeIfNeeded("Confirmed")
+  title <- testResultPlotTitle(FALSE, length(stateChoices) == 0, movingAvg)
+
   if (is.null(stateChoices)) {
     if (movingAvg) {
-      title <- "COVID Test Positivity Trend for US as a whole, 7 day moving average"
       theCaseData <- US_Confirmed_A7
       theTestData <- US_People_Tested_A7
     } else {
-      title <- "COVID Test Positivity Trend for US as a whole"
       theCaseData <- US_Confirmed
       theTestData <- US_People_Tested
     }
   } else {
     if (movingAvg) {
-      title <- "COVID Test Positivity Trends for Selected States, 7 day moving average"
       theCaseData <- US_State_Confirmed_A7
       theTestData <- US_State_People_Tested_A7
     } else {
-      title <- "COVID Test Positivity Trends for Selected States"
       theCaseData <- US_State_Confirmed
       theTestData <- US_State_People_Tested
     }
