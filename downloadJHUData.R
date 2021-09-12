21d3798c28650e8ff8219a45ee219d19a2fe61ff
Dean Bandes <deanlie@yahoo.com>
2021-09-11 22:33:15 -0400

    Remove some cruft from downloadJHUData.R

--- downloadJHUData.R
+++ downloadJHUData.R
@@ -69,66 +69,68 @@ library(tidyverse)
 library(RCurl)
 
 source("dateFormatRoutines.R")
+source("URLFunctions.R")
+source("pathnameFunctions.R")
 source("columnUtilities.R")
+# 
+# GithubUserContent <- function() {
+#   return("https://raw.githubusercontent.com/")
+# }
+# 
+# CSSEGICOVID19Master <- function() {
+#   return("CSSEGISandData/COVID-19/master/")
+# }
+# 
+# GovexMaster <- function() {
+#   return("govex/COVID-19/master/")
+# }
+# 
+# GxTablesUSData <- function() {
+#   return("data_tables/vaccine_data/us_data/")
+# }
 
-GithubUserContent <- function() {
-  return("https://raw.githubusercontent.com/")
-}
-
-CSSEGICOVID19Master <- function() {
-  return("CSSEGISandData/COVID-19/master/")
-}
-
-GovexMaster <- function() {
-  return("govex/COVID-19/master/")
-}
-
-GxTablesUSData <- function() {
-  return("data_tables/vaccine_data/us_data/")
-}
-
-JHU_repository <- function() {
-  paste(GithubUserContent(),
-        CSSEGICOVID19Master(),
-        "csse_covid_19_data/",
-        sep = "")
-}
-
-filenameOfStateLevelDataForDate <- function(aDate) {
-  paste(jhuFileDateString(aDate), ".csv", sep="")
-}
-
-pathnameOfStateLevelUpdateDataForDate <- function(aDate) {
-  paste("./DATA/",filenameOfStateLevelDataForDate(aDate), sep = "")
-}
-
-updateStateLevelDataForDate_URL <- function(aDate) {
-  paste(JHU_repository(),
-        "csse_covid_19_daily_reports_us/",
-        filenameOfStateLevelDataForDate(aDate),
-        sep = "")
-}
-
-updateData_URL <- function() {
-  updateStateLevelDataForDate_URL(expectedLatestUpdateDataDate())
-}
-
-JHUDailyStateDataURLForDate <- function(aDate) {
-  U_out <- paste(JHU_repository(),
-                 "csse_covid_19_daily_reports/",
-                 filenameOfStateLevelDataForDate(aDate),
-                 sep = "")
-}
-
-# Synthesize the URL of the Johns Hopkins time series data file
-# Arguments: type = {"confirmed", "deaths"}, locale = {"US", "global"}
-TS_URL <- function(type, locale) {
-  U_out <- paste(JHU_repository(),
-                 "csse_covid_19_time_series/",
-                 "time_series_covid19_",
-                 type, "_", locale, ".csv",
-                 sep = "")
-}
+# JHU_repository <- function() {
+#   paste(GithubUserContent(),
+#         CSSEGICOVID19Master(),
+#         "csse_covid_19_data/",
+#         sep = "")
+# }
+# 
+# filenameOfStateLevelDataForDate <- function(aDate) {
+#   paste(jhuFileDateString(aDate), ".csv", sep="")
+# }
+# 
+# pathnameOfStateLevelUpdateDataForDate <- function(aDate) {
+#   paste("./DATA/",filenameOfStateLevelDataForDate(aDate), sep = "")
+# }
+# 
+# updateStateLevelDataForDate_URL <- function(aDate) {
+#   paste(JHU_repository(),
+#         "csse_covid_19_daily_reports_us/",
+#         filenameOfStateLevelDataForDate(aDate),
+#         sep = "")
+# }
+# 
+# updateData_URL <- function() {
+#   updateStateLevelDataForDate_URL(expectedLatestUpdateDataDate())
+# }
+# 
+# JHUDailyStateDataURLForDate <- function(aDate) {
+#   U_out <- paste(JHU_repository(),
+#                  "csse_covid_19_daily_reports/",
+#                  filenameOfStateLevelDataForDate(aDate),
+#                  sep = "")
+# }
+# 
+# # Synthesize the URL of the Johns Hopkins time series data file
+# # Arguments: type = {"confirmed", "deaths"}, locale = {"US", "global"}
+# TS_URL <- function(type, locale) {
+#   U_out <- paste(JHU_repository(),
+#                  "csse_covid_19_time_series/",
+#                  "time_series_covid19_",
+#                  type, "_", locale, ".csv",
+#                  sep = "")
+# }
 
 # Good for daily update, not so much if you lost a day
 #   https://raw.githubusercontent.com/"
