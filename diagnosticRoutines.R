7c869746c1f8f34f05855bdd81ffcb44edb25680
Dean Bandes <deanlie@yahoo.com>
2021-10-01 11:27:04 -0400

    Update diagnosticRoutines.R
    Default search so returnEndsOfTibbleRow, conciseEndsOfTibbleRow work with more input tibbles without having to specify the desired key

--- diagnosticRoutines.R
+++ diagnosticRoutines.R
@@ -16,7 +16,7 @@ returnEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
     }
   }
   
-  if (is.null(theKey)) {
+  if (is.null(keyValue)) {
     theData <- filter(aTibble,
                       (.data[[theKey]] == "US") |
                         (.data[[theKey]] == "Massachusetts, US") |
@@ -29,6 +29,8 @@ returnEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
   preEnd <- (theLength + 1) - nLast
 
   if (traceThisRoutine) {
+    nDigits <- getOption("digits")
+    options(digits = 6)
     cat(file = stderr(), myPrepend, "Ends of tibble", itsName, "\n")
     cat(file = stderr(), myPrepend, "key =", theKey, "keyValue =", keyValue, "\n")
     cat(file = stderr(), myPrepend, "Length:", theLength, "\n")
@@ -36,6 +38,7 @@ returnEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
     cat(file = stderr(), myPrepend, "First data:", paste(theData[1,1:nFirst]), "\n")
     cat(file = stderr(), myPrepend, "Last cols:", paste(names(theData)[preEnd:theLength]), "\n")
     cat(file = stderr(), myPrepend, "Last data:", paste(as.integer(theData[1,preEnd:theLength])), "\n")
+    options(digits = nDigits)
   }
   
   if (traceThisRoutine) {
@@ -47,7 +50,7 @@ returnEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
 
 conciseEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
                                    theKey = "Combined_Key",
-                                   keyValue = "Massachusetts, US",
+                                   keyValue = NULL,
                                    nFirst = 2, nLast = 2,
                                    traceThisRoutine = FALSE,
                                    prepend = "") {
@@ -55,19 +58,29 @@ conciseEndsOfTibbleRow <- function(aTibble, itsName = "<?>",
   if (traceThisRoutine) {
     cat(file = stderr(), prepend, "Entered conciseEndsOfTibbleRow\n")
   }
-  
-  theData <- filter(aTibble, .data[[theKey]] == {keyValue})
+  if (is.null(keyValue)) {
+    theData <- filter(aTibble,
+                      (.data[[theKey]] == "US") |
+                        (.data[[theKey]] == "Massachusetts, US") |
+                        (.data[[theKey]] == "Suffolk, Massachusetts, US"))
+  } else {
+    theData <- filter(aTibble, .data[[theKey]] == {keyValue})
+  }
+
   theLength <- dim(theData)[2]
   preEnd <- (theLength + 1) - nLast
   
   if (traceThisRoutine) {
+    nDigits <- getOption("digits")
+    options(digits = 6)
     cat(file = stderr(), myPrepend, "Tibble", itsName, "has", theLength, "cols\n")
     cat(file = stderr(), myPrepend, "Cols:",
         paste(names(theData)[c(1:nFirst)]), "...",
         paste(names(theData)[c(preEnd:theLength)]),"\n")
     cat(file = stderr(), myPrepend, "Data:",
-        paste(theData[1,c(1:nFirst)]), "...",
-        paste(theData[1,c(preEnd:theLength)]), "\n")
+        paste(sprintf("%10.2f", theData[1,c(1:nFirst)])), "...",
+        paste(sprintf("%10.2f", theData[1,c(preEnd:theLength)])), "\n")
+    options(digits = nDigits)
   }
   
   if (traceThisRoutine) {
